b57de099039f2c30bba82cc25f589cbc46178c77
Angelika Blackburn <esonyei@gmail.com>
2021-02-02 04:14:31 -0500

    add dish toggle displayed and animated

--- src/components/operatorDashboard/OperatorMenu.js
+++ src/components/operatorDashboard/OperatorMenu.js
@@ -7,20 +7,71 @@ const initialState = {
 	itemPrice: "",
 };
 
-export default function OperatorMenu({ menu }) {
+export default function OperatorMenu(props) {
+	console.log(props);
 	const [edit, setEdit] = useState(false);
+	const [dish, setDish] = useState(false);
+	const [newMenu, setNewMenu] = useState([initialState]);
 
 	const menuEdits = () => {
 		return setEdit(!edit);
 	};
 
+	const addDish = () => {
+		return setDish(!dish);
+	};
+
+	const updateMenu = (menu) => {
+		setNewMenu(props.menu);
+	};
+
 	return (
 		<div className="operator__dashboard__menu">
-			<button onClick={menuEdits} className="operator__dashboard__menu__btn">
-				Edit the Menu
-			</button>
+			<div className="operator__dashboard__menu__btn-row">
+				<button onClick={menuEdits} className="operator__dashboard__menu__btn">
+					Edit the Menu
+				</button>
+				<button onClick={addDish} className="operator__dashboard__menu__btn">
+					Add a New Dish
+				</button>
+			</div>
+			{dish === true ? (
+				<div>
+					<form className="operator__dashboard__form">
+						<label className="operator__dashboard__form__label">Name:</label>
+						<input
+							name="name"
+							type="text"
+							id="name"
+							placeholder="Enter dish name"
+							className="operator__dashboard__form__input"
+						/>
+						<label className="operator__dashboard__form__label">
+							Description:
+						</label>
+						<input
+							name="description"
+							type="text"
+							id="description"
+							placeholder="Enter dish's description"
+							className="operator__dashboard__form__input"
+						/>
+						<label className="operator__dashboard__form__label">Price:</label>
+						<input
+							name="price"
+							type="number"
+							id="price"
+							placeholder="Enter dish's sell price"
+							className="operator__dashboard__form__input"
+						/>
+					</form>
+				</div>
+			) : (
+				""
+			)}
+
 			{edit === true ? (
-				menu.map((item) => {
+				newMenu.map((item) => {
 					return (
 						<div
 							key={uuid()}
@@ -36,6 +87,9 @@ export default function OperatorMenu({ menu }) {
 								<p className="operator__dashboard__menu__container__price">
 									{item.itemPrice}
 								</p>
+								<button className="operator__dashboard__menu__btn">
+									Delete
+								</button>
 							</div>
 						</div>
 					);
