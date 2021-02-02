c9385deebc0045903d609081e1d3071490ebdda1
Angelika Blackburn <esonyei@gmail.com>
2021-02-02 00:33:02 -0500

    death to eslint

--- src/components/foodTruck/FoodTruck.js
+++ src/components/foodTruck/FoodTruck.js
@@ -8,50 +8,47 @@ import { axiosWithAuth } from "../../utils/axiosWithAuth";
 import OperatorDashboard from "./OperatorDashboard";
 
 const FoodTruck = () => {
-
-	const {push} = useHistory()
+	const { push } = useHistory();
 
 	const getFoodTruck = () => {
 		axiosWithAuth()
-		.get(`api/trucks/user${userId}`)
-		.then(res => {
-			console.log('GET FOOD TRUCK RES: ', res)
-		})
-		.catch(err => {
-			console.log('GET FOOD TRUCK ERROR: ', err)
-		})
-	}
+			.get(`api/trucks/user${userId}`)
+			.then((res) => {
+				console.log("GET FOOD TRUCK RES: ", res);
+			})
+			.catch((err) => {
+				console.log("GET FOOD TRUCK ERROR: ", err);
+			});
+	};
 
 	const editTruck = () => {
 		axiosWithAuth()
-		.post(`api/trucks/user${userId}/`)
-		.then(res => {
-			console.log('POST FOOD TRUCK RES: ', res)
-			push('/update-truck/${}')
-		})
-		.catch(err => {
-			console.log('POST FOOD TRUCK ERROR: ', err)
-	})
+			.post(`api/trucks/user${userId}/`)
+			.then((res) => {
+				console.log("POST FOOD TRUCK RES: ", res);
+				push("/update-truck/${}");
+			})
+			.catch((err) => {
+				console.log("POST FOOD TRUCK ERROR: ", err);
+			});
 
-	const deleteTruck = (event) => {
-		event.preventDefault()
-		axiosWithAuth()
-		.delete(`api/trucks/user${userId}/${truckId}`)
-		.then(res => {
-			console.log('DELETE FOOD TRUCK RES: ', res)
-			push('/operator-dashboard')
-		})
-		.catch(err => {
-			console.log('DELETE FOOD TRUCK ERROR: ', err)
-		})
-	}
-
+		const deleteTruck = (event) => {
+			event.preventDefault();
+			axiosWithAuth()
+				.delete(`api/trucks/user${userId}/${truckId}`)
+				.then((res) => {
+					console.log("DELETE FOOD TRUCK RES: ", res);
+					push("/operator-dashboard");
+				})
+				.catch((err) => {
+					console.log("DELETE FOOD TRUCK ERROR: ", err);
+				});
+		};
+	};
 
 	return (
 		<div className="foodTruck__container">
-			<div>
-				{operator === true ? <OperatorDashboard/> : ''}
-			</div>
+			<div>{operator === true ? <OperatorDashboard /> : ""}</div>
 			<h1 className="foodTruck__container__title">Find a truck near you!</h1>
 			{dummyData.map((truck) => {
 				return (
@@ -64,18 +61,16 @@ const FoodTruck = () => {
 						</p>
 						<Menu menu={truck} />
 
-					<div className='buttons'>
-						<button className='dashbord-button'>Operator Dashboard</button>
-						<button className="edit-button">Edit</button>
-						<button className="delete-button">Delete</button>
+						<div className="buttons">
+							<button className="dashbord-button">Operator Dashboard</button>
+							<button className="edit-button">Edit</button>
+							<button className="delete-button">Delete</button>
+						</div>
 					</div>
-
-					</div>
-
 				);
 			})}
 		</div>
 	);
-}
+};
 
-export default FoodTruck
+export default FoodTruck;
